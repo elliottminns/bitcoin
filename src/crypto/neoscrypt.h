64463c472c20fcaebcf32f4472111799639406a6
Elliott Minns <elliott.minns@me.com>
2018-06-18 23:23:53 +0100

    Added in include guards to neoscrypt.h

--- src/crypto/neoscrypt.h
+++ src/crypto/neoscrypt.h
@@ -1,3 +1,5 @@
+#ifndef NEOSCRYPT_H
+#define NEOSCRYPT_H
 extern "C" {
 void neoscrypt(const unsigned char *input, 
                unsigned char *output,
@@ -10,8 +12,6 @@ void neoscrypt(const unsigned char *input,
 
 typedef uint8_t hash_digest[SCRYPT_HASH_DIGEST_SIZE];
 
-#define ROTL32(a,b) (((a) << (b)) | ((a) >> (32 - b)))
-#define ROTR32(a,b) (((a) >> (b)) | ((a) << (32 - b)))
 
 #define U8TO32_BE(p) \
     (((uint32_t)((p)[0]) << 24) | ((uint32_t)((p)[1]) << 16) | \
@@ -25,6 +25,4 @@ typedef uint8_t hash_digest[SCRYPT_HASH_DIGEST_SIZE];
     U32TO8_BE((p),     (uint32_t)((v) >> 32)); \
     U32TO8_BE((p) + 4, (uint32_t)((v)      ));
 
-
-
-
+#endif
