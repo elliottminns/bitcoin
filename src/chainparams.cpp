b08bcb7b3ab5462da3f3da49b3f2ddbe17ad8845
Elliott Minns <elliott.minns@me.com>
2018-06-19 22:07:41 +0100

    Initial seeds in chain params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,6 +133,8 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they dont support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
+        vSeeds.emplace_back("80.211.145.197");
+        vSeeds.emplace_back("80.211.146.33");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,80);
